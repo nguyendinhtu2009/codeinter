ce3fd7bf4eb87b5b44d1c9fd4c58a1d9c0a721fd
nguyendinhtu2009 <42923521+nguyendinhtu2009@users.noreply.github.com>
2018-09-04 14:51:14 +0700

    55555

--- application/controllers/Addproduct.php
+++ application/controllers/Addproduct.php
@@ -30,16 +30,7 @@ class Addproduct extends CI_Controller
 				"product_msp"=>$this->input->post('maproduct'),
 				"product_mt"=>$this->input->post('mtprodcut')
 			);
-			if ($_FILES['files']['name']) {
-				$config['upload_path'] = './uploads/uploads_product';
-				$config['allowed_types'] = 'gif|jpg|png';
-				$this->load->library("upload", $config);
-				$this->upload->initialize($config);
-				if ($this->upload->do_upload('files')) {
-					$fileData = $this->upload->data();
-					$dataInsert['product_img'] = $fileData['file_name'];
-				}
-			}
+
 				$this->AddproductModel->insertProduct($data_insert);
 				echo "<script>alert('Đăng Sản Phẩm Thành Công');</script>";
 				redirect(base_url()."addproduct/listproduct");
